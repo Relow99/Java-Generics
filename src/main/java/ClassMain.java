d46d87993954cf1a064bf682dca59f07239259d8
Tshwarelo <59955727+Relow99@users.noreply.github.com>
2020-06-15 15:57:52 +0200

    update

--- src/main/java/ClassMain.java
+++ src/main/java/ClassMain.java
@@ -1,19 +1,18 @@
 public class ClassMain {
     public static void main (String[] args)
     {
-        // instance of String type
+        // instance of Integer type
         Integer[] intArray = { 6, 321, 96, 9009 };
-        String[] stringArray = {"Though", "I", "Walk", "Through", "The" , "Valley", "Of", "Shadow" };
-
         System.out.println("Integer Array Contains:");
         GenericClass.ArrayContent(intArray);
 
+        // instance of Integer type
+        String[] stringArray = {"Though", "I", "Walk", "Through", "The" , "Valley", "Of", "Shadow" };
         System.out.println("String Array Contains:");
         GenericClass.ArrayContent(stringArray);
 
         GenericClass<String> string = new GenericClass<String>("Test");
         System.out.println(string.obj);
-        // instance of Integer type
         GenericClass<Integer> number = new GenericClass<Integer>(1000);
         System.out.println(number.obj);
     }
